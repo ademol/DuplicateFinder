69540f7b77e20967e946c79ae9b1fccc89faf6d2
Arjan de Mol <ademol@users.noreply.github.com>
2020-05-17 20:04:12 +0200

    Add GetDirectory & GetFiles methods

--- DuplicateFinder/FileWalker.cs
+++ DuplicateFinder/FileWalker.cs
@@ -1,109 +1,125 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 
 namespace DuplicateFinder
 {
-    public class FileWalker
+    public interface IFileWalker
     {
+        //public void RecurseDirectories(string path);
+        public string[] GetDirectories(string path);
+    }
+
+    public class FileWalker : IFileWalker
+    {
+        private readonly ICompareService _compareService;
+        private readonly IFileSystem _fileSystem;
         private readonly IOutput _output = Output.Instance;
 
-        public static List<string> EnumerateFileSystems()
+        public FileWalker(ICompareService compareService, IFileSystem fileSystem)
         {
-            return Directory.GetLogicalDrives().ToList();
+            _compareService = compareService;
+            _fileSystem = fileSystem;
         }
 
 
-        public static IEnumerable<string> EnumerateDirectories(string path)
+        public string[] GetDirectories(string path)
         {
-            IEnumerable<string> result = new List<string>();
-            try
-            {
-                result = Directory.EnumerateDirectories(path);
-            }
-            catch (Exception e)
-            {
-                Output.Instance.Write($"[{e.Message}");
-            }
-
-            return result.ToList();
+            return _fileSystem.Directory.GetDirectories(path);
         }
 
-        public static IEnumerable<string> EnumerateFiles(string path)
+        public string[] GetFiles(string path)
         {
-            IEnumerable<string> result = new List<string>();
-            try
-            {
-                result = Directory.EnumerateFiles(path);
-            }
-            catch (Exception e)
-            {
-                Output.Instance.Write($"[{e.Message}");
-            }
-
-            return result.ToList();
-        }
-
-        private static bool IsSpecialPath(string path)
-        {
-            var linuxSystemPaths = new List<string> {"/sys", "/proc", "/run", "/dev"};
-
-            var specialPaths = linuxSystemPaths;
-
-            return specialPaths.Contains(path);
-        }
-
-        private static bool FilterByExtensions(string fileName)
-        {
-            var extensionsToFilterOut = new List<string>
-            {
-                ".mp4", ".jpg", ".mp3", ".doc", ".webm", ".odg"
-            };
-
-            var extension = Path.GetExtension(fileName).ToLower();
-
-            return extensionsToFilterOut.Select(x => x.ToLower()).Contains(extension);
+            return _fileSystem.Directory.GetFiles(path);
         }
 
 
-        public void RecurseDirectories(string path)
-        {
-            if (IsSpecialPath(path))
-            {
-                _output.Write($"[{path}] special: skipping");
-                return;
-            }
 
-            var subDirs = EnumerateDirectories(path);
-            foreach (var dir in subDirs)
-            {
-                RecurseDirectories(dir);
-            }
+//
+//
+//
+//
+//
+//
+//         private static bool IsSpecialPath(string path)
+//         {
+//             var linuxSystemPaths = new List<string> {"/sys", "/proc", "/run", "/dev"};
+//
+//             var specialPaths = linuxSystemPaths;
+//
+//             return specialPaths.Contains(path);
+//         }
+//
+//         private static bool FilterByExtensions(string fileName)
+//         {
+//             var extensionsToFilterOut = new List<string>
+//             {
+//                 ".mp4", ".jpg", ".mp3", ".doc", ".webm", ".odg"
+//             };
+//
+//             var extension = Path.GetExtension(fileName).ToLower();
+//
+//             return extensionsToFilterOut.Select(x => x.ToLower()).Contains(extension);
+//         }
+//
+//
+//         public void RecurseDirectories(string path)
+//         {
+//             Console.WriteLine(path);
+//             if (IsSpecialPath(path))
+//             {
+//                 _output.Write($"[{path}] special: skipping");
+//                 return;
+//             }
+//
+//             try
+//             {
+//                 var subDirs = _fileSystem.Directory.GetDirectories(path);
+//                 foreach (var dir in subDirs)
+//                 {
+//                     RecurseDirectories(dir);
+//                 }
+//             }
+//             catch (Exception e)
+//             {
+//                 _output.Write(e.Message);
+//             }
+//
+//             var files = new List<string>();
+//             try {
+//                  files = _fileSystem.Directory.GetFiles(path).ToList();
+//             }
+//             catch (Exception e)
+//             {
+//                 _output.Write(e.Message);
+//             }
+//
+//             foreach (var file in files)
+//             {
+//                 // if (!FilterByExtensions(file))
+//                 // {
+//                 //     continue;
+//                 // }
+//
+// //                var fileInfo = new FileInfo(file);
+//                 var fileInfo = _fileSystem.FileInfo.FromFileName(file);
+//
+//                 try
+//                 {
+//               //      if (fileInfo.Length == 0) continue;
+//
+//                     var fd = new FileDetail(file);
+//                     _compareService.CheckForDuplicates(fd);
+//                     _compareService.AddFile(fd);
+//                 }
+//                 catch (Exception e)
+//                 {
+//                     _output.Write(e.Message);
+//                 }
+//             }
+//         }
 
-            var files = EnumerateFiles(path);
-            foreach (var file in files)
-            {
-                if (!FilterByExtensions(file))
-                {
-                    continue;
-                }
-
-                var fileInfo = new FileInfo(file);
-
-                if (fileInfo.Length == 0) continue;
-
-                try
-                {
-                    var fd = new FileDetail(file);
-                    CompareService.CheckForDuplicates(fd);
-                    CompareService.AddFile(fd);
-                }
-                catch (Exception e)
-                {
-                    _output.Write(e.Message);
-                }
-            }
-        }
     }
 }
--- DuplicateFinderTests/FileWalkerTests.cs
+++ DuplicateFinderTests/FileWalkerTests.cs
@@ -1,46 +1,110 @@
+using System.IO.Abstractions;
+using DeepEqual.Syntax;
 using DuplicateFinder;
+using NSubstitute;
 using Xunit;
 
 namespace DuplicateFinderTests
 {
     public class FileWalkerTests
     {
-        [Fact]
-        public void EnumerateFileSystems()
+        private readonly IFileSystem _fileSystem;
+        private readonly ICompareService _compareService;
+        private readonly FileWalker _fileWalker;
+
+        public FileWalkerTests()
         {
-            // Given
-            // When
-            var drives = FileWalker.EnumerateFileSystems();
-            
-            // Then
-            Assert.NotNull(drives);
+            _compareService = Substitute.For<ICompareService>();
+            _fileSystem = Substitute.For<IFileSystem>();
+            _fileWalker = new FileWalker(_compareService, _fileSystem);
         }
 
 
-        [Fact]
-        public void EnumerateDirectories()
+        [Theory]
+        [InlineData("/", new [] { "/tmp", "/etc", "/home"})]
+        [InlineData("/tmp", null)]
+        public void GetDirectories(string path, string[] expectedDirectories)
         {
             // Given
-            var fileWalker = new FileWalker();
-            
+            _fileSystem.Directory.GetDirectories(path).Returns(expectedDirectories);
+
             // When
-            var directories = FileWalker.EnumerateDirectories("C:\\");
-            
+           var actual = _fileWalker.GetDirectories(path);
+
             // Then
-            Assert.NotNull(directories);
+            actual.ShouldDeepEqual(expectedDirectories);
         }
 
-        [Fact]
-        public void EnumerateFiles()
+        [Theory]
+        [InlineData("/", new [] { "/vmlinuz"})]
+        [InlineData("/tmp", null)]
+        public void GetFiles(string path, string[] expectedFiles)
         {
             // Given
-            var fileWalker = new FileWalker();
-            
+            _fileSystem.Directory.GetFiles(path).Returns(expectedFiles);
+
             // When
-            var files = FileWalker.EnumerateFiles("C:\\");
-            
+            var actual = _fileWalker.GetFiles(path);
+
             // Then
-            Assert.NotNull(files);
+            actual.ShouldDeepEqual(expectedFiles);
         }
+
+
+        // [Fact]
+        // public void RecursePath()
+        // {
+        //     // Given
+        //     const string rootPath = "/";
+        //     const string homePath = "/home";
+        //     const string userPath = "/home/user1";
+        //
+        //     var rootSubDirs = new[] {homePath, "/etc", "/tmp"};
+        //     var homeSubDirs = new[] {userPath};
+        //
+        //     _fileSystem.Directory.GetDirectories(rootPath).Returns(rootSubDirs);
+        //     _fileSystem.Directory.GetDirectories(homePath).Returns(homeSubDirs);
+        //
+        //     var filesInRootPath = new string[] {"/vmlinuz"};
+        //     var filesInUserPath = new string[] {"/home/user1/.profile", "/home/user1/Readme.txt"};
+        //
+        //     _fileSystem.Directory.GetFiles(rootPath).Returns(filesInRootPath);
+        //     _fileSystem.Directory.GetFiles(userPath).Returns(filesInUserPath);
+        //
+        //     // when
+        //     var actual = RecursePath(rootPath);
+        // }
+
+        // [Fact]
+        // public void RecursePath()
+        // {
+        //     // Given
+        //     var rootPaths = new[] {"/home", "/tmp"};
+        //     const string rootFile = "/vmlinuz";
+        //     var rootFiles = new[] {rootFile};
+        //     const string homeDir = "/home/user";
+        //     var homeDirs = new[] {homeDir};
+        //     const string userFileA = "/home/user/.profile";
+        //     const string userFileB = "/home/user/userfile";
+        //     var userHomeDirFiles = new[] {userFileA, userFileB};
+        //
+        //     _fileSystem.Directory.GetDirectories("/").Returns(rootPaths);
+        //     _fileSystem.Directory.GetDirectories("/home").Returns(homeDirs);
+        //
+        //     _fileSystem.Directory.GetFiles("/").Returns(rootFiles);
+        //     _fileSystem.Directory.GetFiles("/home/user").Returns(userHomeDirFiles);
+        //
+        //     _fileSystem.FileInfo.FromFileName(userFileA).Returns(new FileInfoWrapper(_fileSystem, new FileInfo(userFileA)));
+        //     _fileSystem.FileInfo.FromFileName(userFileB).Returns(new FileInfoWrapper(_fileSystem, new FileInfo(userFileB)));
+        //
+        //     // When
+        //     _fileWalker.RecurseDirectories("/");
+        //
+        //     // Then
+        //
+        //     _compareService.Received(1).AddFile(Arg.Is<FileDetail>(s => s.FileName.Equals(rootFile)));
+        //     _compareService.Received(1).AddFile(Arg.Is<FileDetail>(s => s.FileName.Equals(userFileA)));
+        //     _compareService.Received(1).AddFile(Arg.Is<FileDetail>(s => s.FileName.Equals(userFileB)));
+        // }
     }
 }
